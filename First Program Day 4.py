b3bf326a116e968152a18e9b389c7ac0e3f93b21
Vipul Sharma <116291385+vipulsharma17@users.noreply.github.com>
2024-07-09 02:43:11 +0530

    Add files via upload

---
+++ First Program Day 4.py
@@ -0,0 +1,19 @@
+#These are few first lines that i thought i should execute after learning the basics
+
+#it is important to write words inside inverted comma to let the Interpreter know its a string !
+print("Hello World")
+
+#--------------------------------
+
+# it prints number 5 as int value
+print(5)
+
+#--------------------------------
+
+# if you write int value inside the inverted comma it would consider it as string !
+print('5')
+
+#--------------------------------
+
+# Some random numbers multiplied
+print(23232*2342)
