4a8e9fcce7f176794c64f4eb8b6c565162099801
Vipul Sharma <116291385+vipulsharma17@users.noreply.github.com>
2024-07-11 21:58:42 +0530

    Delete Exercise 1 Calculator using Python Day 7.py

--- Exercise 1 Calculator using Python Day 7.py
+++
@@ -1,9 +0,0 @@
-a=15
-b=7
-print("the Additon of", a,'+',b, "is: ",a+b)
-print("the Subtraction of",a,"-",b,"is: ",a-b)
-print("the Multiplication of", a,'*',b, "is: ",a*b)
-print("the Division of",a,"/",b,"is: ",a/b)
-print("the Modulus of", a,'%',b, "is: ",a%b)
-print("the exponential of",a,"**",b,"is: ",a**b)
-print("the floor division of",a,"//",b,"is: ",a//b)
