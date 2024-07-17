0b6c152ecc179f8bd100512e69a89bd7f7131888
Vipul Sharma <116291385+vipulsharma17@users.noreply.github.com>
2024-07-17 13:54:14 +0530

    Add files via upload

---
+++ Strings and For loop Day 11.py
@@ -0,0 +1,19 @@
+#strings in python are written between single or double comma
+
+print('He said,\"I want to eat an apple\"')
+
+#we can loop the string using for loop,which we would learn in upcoming sessions
+
+a=("Hello how are you") 
+for hello in a:
+    print(hello)
+
+hello="hi"
+sum=0
+for i in range(10):
+    sum=sum+i
+    print(sum)
+
+name='vipul'
+for i in name:
+    print(i) 
