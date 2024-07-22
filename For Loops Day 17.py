535b6aae941517e7281ef96e9c7e4491a15b6360
Vipul Sharma <116291385+vipulsharma17@users.noreply.github.com>
2024-07-22 11:56:47 +0530

    Add files via upload

---
+++ For Loops Day 17.py
@@ -0,0 +1,18 @@
+# for loops in python are used to repeate a condition for n number of times until it is satisfied
+# for loop can iterate over a sequence of objects
+name="Vipul"
+for i in name:
+    print (i,end='u')
+
+colors=["green","yellow","blue","red"]
+for i in colors:
+    print(i)
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
+
+for k in range(3):
+    print(k)
