cfc95bec1c8cece861eed53f62dc170aaa1bf445
Vipul Sharma <116291385+vipulsharma17@users.noreply.github.com>
2024-08-10 15:40:42 +0530

    Add files via upload

---
+++ For Loop with else Day 35.py
@@ -0,0 +1,16 @@
+for no in range(4):
+        if no==2:
+            break
+        print(no)
+
+else:
+    print("Sorry")  
+
+i=0
+while i<7:
+    print(i)
+    i=i+1
+    if i==9:
+        break
+else:
+    print("Sorry no i")
